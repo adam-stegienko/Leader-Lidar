9a5691660432fa07eb712c9261bc992a5af30c3b
Adam Stegienko <adam.stegienko1@gmail.com>
2022-11-01 15:25:40 +0100

    Before merge

--- copy_lab.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SECRET_KEY="adam-lab.pem"
-PUBLIC_DNS="ubuntu@ec2-3-125-51-254.eu-central-1.compute.amazonaws.com"
-
-scp -i $SECRET_KEY -r ./leader-lidar $PUBLIC_DNS:~/
-ssh -i $SECRET_KEY $PUBLIC_DNS
